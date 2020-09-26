49767590d4e934bebd64af3244231ff28be176ff
Akbar <akbar@cybertekschool.com>
2020-09-26 11:54:15 -0400

    Moudules | remainder operator practice

---
+++ src/a_review/week2/AppleDivision.java
@@ -0,0 +1,24 @@
+package a_review.week2;
+
+public class AppleDivision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int appleCount = 99 ; 
+		
+		int pplCount = 5 ; 
+		
+		// generate result as 
+		// each person will get <count> 
+		// and <..> apple will remain 
+		
+		System.out.println("each person will get apple :  "  +  appleCount / pplCount  ); 
+		System.out.println("remaining apple count is   :  "  +  appleCount % pplCount );
+		
+		
+		
+
+	}
+
+}
