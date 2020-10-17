39c0aca6625828869c00877f16e388d6907ead4b
Akbar <akbar@cybertekschool.com>
2020-10-17 10:49:03 -0400

    Week 5 : method review starter

---
+++ src/a_review/week5/MethodCallInDifferentClass.java
@@ -0,0 +1,12 @@
+package a_review.week5;
+
+public class MethodCallInDifferentClass {
+
+	public static void main(String[] args) {
+		// How do I reuse the static methods I created in other class 
+		
+		sayHello();
+
+	}
+
+}
