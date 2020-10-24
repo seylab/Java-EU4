1f2e683e622f9e2f5e665b25f7918ead3c337cd5
Akbar <akbar@cybertekschool.com>
2020-10-24 14:26:44 -0400

    Week 6 : String practice

--- src/a_review/week6/StringMethodsPractice.java
+++ src/a_review/week6/StringMethodsPractice.java
@@ -33,6 +33,23 @@ public class StringMethodsPractice {
 		message =   message.toUpperCase(); 
 		System.out.println("after using uppercase method in above line \n  "+  message );
 		
+		String str = "abc " ; 
+		System.out.println("str has character count : " + str.length() );
+		
+		System.out.println("is <abc > equal <abc> " + str.equals("abc")  );
+		// what if i did not care about the leading or training space 
+		
+		// save the trimmed result in new variable 
+		String str2 =  str.trim();   //abc
+		System.out.println("the result after trimming "  +  str2.equals("abc")    );
+		
+		// compare str2 with ABC without caring about the case. 
+		System.out.println("check str2 is ABC caseInsensitive   " 
+											+ str2.equalsIgnoreCase("ABC")   );
+		
+		// contain :  partial check 
+		// 
+		System.out.println("Does str1 contains letter a "  +   str.contains("a")    );
 		
 		
 		
