e907533fb37e0b2f158d26f45063a758e56d744f
cybertekschool <github@cybertekschool.com>
2020-11-21 12:43:34 -0500

    week 10

--- src/a_review/week10/StringPractice6.java
+++ src/a_review/week10/StringPractice6.java
@@ -4,7 +4,28 @@ public class StringPractice6 {
 	
 	public static void main(String[] args) {
 		
-		String str = "ababaaac";
+		String str = "ababaaacbbdaaae";
+		String uniques = "";
+		
+		for(char ch  : str.toCharArray()) {
+			
+			int count = 0;
+			for(char each  : str.toCharArray() ) {
+				if(each == ch) {
+					count++;
+				}
+			}
+			
+			if(count == 1) {
+				uniques += ch;
+			}
+			
+		}
+		
+		
+		System.out.println(uniques);
+		
+		
 		
 		
 		
