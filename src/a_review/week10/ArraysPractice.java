dae8fd5856e11c44bcaf8225c631c97cb86c6439
cybertekschool <github@cybertekschool.com>
2020-11-21 14:26:36 -0500

    Week 10

--- src/a_review/week10/ArraysPractice.java
+++ src/a_review/week10/ArraysPractice.java
@@ -1,5 +1,7 @@
 package a_review.week10;
 
+import java.text.DecimalFormat;
+
 public class ArraysPractice {
 	
 	public static void main(String[] args) {
@@ -19,10 +21,29 @@ public class ArraysPractice {
 		System.out.println("Even Numbers: "+countEven);
 		System.out.println("Odd Numbers: "+countOdd);
 		
+	System.out.println("======================================");
 		
-	/*
-	 come back at 2pm	
-	 */
+		int[] numbers = {10, 20, 5, 4, 3, 2, 1, 40, 30, 45, 1};
+		int max = numbers[0]; // 45
+		int min = numbers[0]; 
+		int sum = 0;
+		
+		for(int each  : numbers) {
+			 max=Math.max (each, max);
+			 min= Math.min(each, min);
+			 sum += each; 
+		}
+		//					10.0  / 3
+		//			       double / int ==> double
+		double average =  (double)sum / numbers.length;
+		
+		DecimalFormat df = new DecimalFormat("0.0000");  
+		
+		System.out.println("Max Number: "+max);
+		System.out.println("Min Number: "+min);
+		System.out.println("Average Number: "+ df.format( average ) ); 
+	
+	
 		
 		
 
---
+++ src/a_review/week10/ArraysPractice2.java
@@ -0,0 +1,15 @@
+package a_review.week10;
+
+public class ArraysPractice2 {
+
+	public static void main(String[] args) {
+		
+		String[] group1 = {};
+		String[] group2 = {};
+		
+		
+		
+		
+	}
+	
+}
