6bac50e15c89b7ae97ceed743964d8257e52dc8b
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2020-12-05 10:34:05 -0500

    Week 12 Review

---
+++ src/a_review/week12/Credentials.java
@@ -0,0 +1,10 @@
+package a_review.week12;
+
+public class Credentials {
+
+        private String userName = "A123456789";
+        private String passWord = "Cybertek123";
+
+
+
+}
---
+++ src/a_review/week12/LogIn.java
@@ -0,0 +1,27 @@
+package a_review.week12;
+
+public class LogIn {
+
+    public static void main(String[] args) {
+
+        // System.out.println(Credentials.userName);
+        Credentials obj1 = new Credentials();
+
+        /*
+        System.out.println("Username entered: "+ obj1.userName); // not visible
+        System.out.println("Password entered: "+ obj1.passWord); // not visible
+
+        System.out.println("=========================================");
+        obj1.userName = "B123456"; // not visible
+        obj1.passWord = "789456"; // not visible
+
+        System.out.println("Username entered: "+ obj1.userName); // not visible
+        System.out.println("Password entered: "+ obj1.passWord); // not visible
+     */
+
+
+
+
+    }
+
+}
